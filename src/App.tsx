5e6599746f3cf84ee21b14f19f8e7b1b0fd8acce
AntonyWhatsup <antonkrykhta@gmail.com>
2025-08-11 23:06:24 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,44 +7,44 @@ import Notification from './components/Notification/Notification';
 import type { Votes, VoteType } from './types/votes';
 
 const App = () => {
-  const [votes, setVotes] = useState<Votes>({
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  });
+ const [votes, setVotes] = useState<Votes>({
+  good: 0,
+  neutral: 0,
+  bad: 0,
+ });
 
-  const handleVote = (type: VoteType) => {
-    setVotes(prevVotes => ({
-      ...prevVotes,
-      [type]: prevVotes[type] + 1,
-    }));
-  };
+ const handleVote = (type: VoteType) => {
+  setVotes(prevVotes => ({
+   ...prevVotes,
+   [type]: prevVotes[type] + 1,
+  }));
+ };
 
-  const resetVotes = () => {
-    setVotes({
-      good: 0,
-      neutral: 0,
-      bad: 0,
-    });
-  };
+ const resetVotes = () => {
+  setVotes({
+   good: 0,
+   neutral: 0,
+   bad: 0,
+  });
+ };
 
-  const totalVotes = votes.good + votes.neutral + votes.bad;
-  const positiveRate = totalVotes
-    ? Math.round((votes.good / totalVotes) * 100)
-    : 0;
-  const canReset = totalVotes > 0;
+ const totalVotes = votes.good + votes.neutral + votes.bad;
+ const positiveRate = totalVotes
+  ? Math.round((votes.good / totalVotes) * 100)
+  : 0;
+ const canReset = totalVotes > 0;
 
-  return (
-    <div className={css.app}>
-      <CafeInfo title="Sip Happens Café" />
-      <VoteOptions onVote={handleVote} onReset={resetVotes} canReset={canReset} />
-      {totalVotes > 0 ? (
-        <VoteStats votes={votes} totalVotes={totalVotes} positiveRate={positiveRate} />
-      ) : (
-        <Notification message="Відгуків немає" />
-      )}
-    </div>
-  );
+ return (
+  <div className={css.app}>
+   <CafeInfo title="Sip Happens Café" />
+   <VoteOptions onVote={handleVote} onReset={resetVotes} canReset={canReset} />
+   {totalVotes > 0 ? (
+    <VoteStats votes={votes} totalVotes={totalVotes} positiveRate={positiveRate} />
+   ) : (
+    <Notification message="Відгуків немає" />
+   )}
+  </div>
+ );
 };
 
 export default App;
