8d3c43dabac12ed71bb8b1f5e795389bdf865db2
AntonyWhatsup <antonkrykhta@gmail.com>
2025-08-11 15:22:48 +0200

    sum

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import CafeInfo from './components/CafeInfo/CafeInfo';
+import VoteOptions from './components/VoteOptions/VoteOptions';
+import VoteStats from './components/VoteStats/VoteStats';
+import Notification from './components/Notification/Notification';
+
+// Визначення типу для параметра 'type'
+type VoteType = 'good' | 'neutral' | 'bad';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  // Явно вказуємо тип для параметра 'type'
+  const handleVote = (type: VoteType) => {
+    switch (type) {
+      case 'good':
+        setGood((prevGood) => prevGood + 1);
+        break;
+      case 'neutral':
+        setNeutral((prevNeutral) => prevNeutral + 1);
+        break;
+      case 'bad':
+        setBad((prevBad) => prevBad + 1);
+        break;
+      default:
+        return;
+    }
+  };
+
+  const handleReset = () => {
+    setGood(0);
+    setNeutral(0);
+    setBad(0);
+  };
+
+  const total = good + neutral + bad;
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <CafeInfo title="Expresso Cafe Feedback" />
+      <VoteOptions onVote={handleVote} onReset={handleReset} />
+      <h2>Statistics</h2>
+      {total > 0 ? (
+        <VoteStats good={good} neutral={neutral} bad={bad} total={total} />
+      ) : (
+        <Notification message="No feedback given" />
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
